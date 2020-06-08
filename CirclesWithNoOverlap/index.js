c87a504ecd8e4e689557096d598a54344a691602
manchandajayant <maanjayant@gmail.com>
2020-06-08 16:23:03 +0200

    prettier config

--- CirclesWithNoOverlap/index.js
+++ CirclesWithNoOverlap/index.js
@@ -1,26 +1,32 @@
-var circles = []
-function setup(){
+var circles = [];
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-  createCanvas(windowWidth,windowHeight)
-
-  for(var i=0;i<50;i++){
- var circle={
- x : random(width),
- y : random(height),
- r : 40
+  for (var i = 0; i < 50; i++) {
+    var circle = {
+      x: random(width),
+      y: random(height),
+      r: 40,
+    };
+    var overlapping = false;
+    for (var j = 0; j < circles.length; i++) {
+      var other = circles[j];
+      var d = dist(circle.x, circle.y, other.x, other.y);
+      if (d < circle.r + other.r) {
+        overlapping = true;
+        break;
+      }
+    }
+    if (!overlapping) {
+      circles.push(circle);
+    }
   }
 
-  circles.push(circle)
-
+  for (var i = 0; i < circles.length; i++) {
+    fill(100, 150, 250, 100);
+    noStroke();
+    ellipse(circles[i].x, circles[i].y, circles[i].r * 2, circles[i].r * 2);
   }
-
-for(var i =0;i<circles.length;i++){
-  fill(100,150,250,100)
-  noStroke()
-  ellipse(circles[i].x, circles[i].y, circles[i].r*2, circles[i].r*2)
-}
 }
 
-function draw(){
-
-}
+function draw() {}
